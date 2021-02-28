9b90adfb000f233c8e32959681a7273488d88ecf
Veronika <v.vasilchykava@gmail.com>
2021-02-28 16:28:27 +0300

    refactor: add styles to footer

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -1,23 +1,45 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Container from '@material-ui/core/Container';
+import Toolbar from '@material-ui/core/Toolbar';
 import Link from '@material-ui/core/Link';
 import GiHubIcon from '@material-ui/icons/GitHub';
 import YoutubeIcon from '@material-ui/icons/YouTube';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  copyright: {
+    flexGrow: 1,
+  },
+  icon: {
+    marginRight: theme.spacing(1),
+  },
+  link: {
+    marginRight: theme.spacing(2),
+  }
+}));
 
 export const Copyright: React.FunctionComponent = () => {
+  const classes = useStyles();
+
   return (
-    <Typography variant='body2' color='textSecondary' align='center'>
-      {'Copyright © '}
-      <GiHubIcon />
-      <Link color='inherit' href='https://github.com/VeronikaVasilchikova/react-game'>
-        Source code
-      </Link>{' '}
-      <YoutubeIcon />
-      <Link color='inherit' href='https://github.com/VeronikaVasilchikova/react-game'>
-        Description
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
+    <AppBar position="static" color="primary">
+      <Container maxWidth="sm">
+        <Toolbar>
+          <Typography variant="body2" color="inherit" className={classes.copyright}>
+            {new Date().getFullYear()} {'Copyright © '}
+          </Typography>
+          <GiHubIcon className={classes.icon}/>
+          <Link color='inherit' href='https://github.com/VeronikaVasilchikova/react-game' className={classes.link}>
+            Source code
+          </Link>{' '}
+          <YoutubeIcon className={classes.icon}/>
+          <Link color='inherit' href='https://github.com/VeronikaVasilchikova/react-game'>
+            Description
+          </Link>{' '}
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
